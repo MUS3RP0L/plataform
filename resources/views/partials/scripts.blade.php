55c94592cf7a6cc9f88e2050014e5322229622e0
erickguis <aleguis@icloud.com>
2016-09-05 16:21:08 -0400

    added bower components

--- resources/views/partials/scripts.blade.php
+++ resources/views/partials/scripts.blade.php
@@ -4,3 +4,10 @@
 <script src="{{ asset('/js/bootstrap.min.js') }}" type="text/javascript"></script>
 
 <script src="{{ asset('/js/app.min.js') }}" type="text/javascript"></script>
+
+	{!! Html::script('bower_components/data-tables/media/js/jquery.dataTables.min.js') !!}
+	{!! Html::script('bower_components/data-tables/media/js/dataTables.bootstrap.min.js') !!}
+    {!! Html::script('bower_components/knockout/dist/knockout.js') !!}
+    {!! Html::script('bower_components/bootstrap-combobox/js/bootstrap-combobox.js') !!}
+    {!! Html::script('bower_components/datePicker/js/bootstrap-datepicker.js') !!}
+    {!! Html::script('bower_components/Chart.js/Chart.js') !!}
