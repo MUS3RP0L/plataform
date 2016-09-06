5c0ba9895b4aaacec33702cfa00c869550f6b031
erickguis <aleguis@icloud.com>
2016-09-06 10:21:54 -0400

    upgraded style and script

--- resources/views/partials/htmlheader.blade.php
+++ resources/views/partials/htmlheader.blade.php
@@ -1,27 +1,26 @@
 <head>
     <meta charset="UTF-8">
-    <title>MUSERPOL</title>
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
     <meta name="csrf-token" content="{{ csrf_token() }}" />
 
-    <link href="{{ asset('/css/bootstrap.css') }}" rel="stylesheet" type="text/css" />
+    <title>MUSERPOL</title>
 
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
 
     <link href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css" rel="stylesheet" type="text/css" />
 
-    <link href="{{ asset('/css/AdminLTE.css') }}" rel="stylesheet" type="text/css" />
+    {!! Html::style('css/AdminLTE.css') !!}
 
-    <link href="{{ asset('/css/skins/skin-red.css') }}" rel="stylesheet" type="text/css" />
+    {!! Html::style('css/skins/skin-red.css') !!}
 
-    <link href="{{ asset('/plugins/iCheck/square/red.css') }}" rel="stylesheet" type="text/css" />
+    {!! Html::style('plugins/iCheck/square/red.css') !!}
 
-    <link rel="stylesheet" href="{{ asset('/plugins/datatables/dataTables.bootstrap.css">
+    {!! Html::style('bower_components/bootstrap/dist/css/bootstrap.min.css') !!}
 
-
-    {{-- {!! Html::style('bower_components/data-tables/media/css/dataTables.bootstrap.min.css') !!} --}}
+    {!! Html::style('bower_components/data-tables/media/css/dataTables.bootstrap.min.css') !!}
 
     {!! Html::style('bower_components/bootstrap-combobox/css/bootstrap-combobox.css') !!}
 
     {!! Html::style('bower_components/datePicker/css/bootstrap-datepicker3.css') !!}
+
 </head>
--- resources/views/partials/scripts.blade.php
+++ resources/views/partials/scripts.blade.php
@@ -1,18 +1,12 @@
 
-<script src="{{ asset('/plugins/jQuery/jquery-2.2.3.min.js') }}"></script>
-
-<script src="{{ asset('/js/bootstrap.min.js') }}" type="text/javascript"></script>
-
-<script src="{{ asset('/plugins/datatables/jquery.dataTables.min.js') }}"></script>
-<script src="{{ asset('/plugins/datatables/dataTables.bootstrap.min.js') }}"></script>
-
-<script src="{{ asset('/js/app.min.js') }}" type="text/javascript"></script>
-
-{{-- {!! Html::script('bower_components/data-tables/media/js/jquery.dataTables.min.js') !!} --}}
-{{-- {!! Html::script('bower_components/data-tables/media/js/dataTables.bootstrap.min.js') !!} --}}
+{!! Html::script('plugins/jQuery/jquery-2.2.3.min.js') !!}
+{!! Html::script('bower_components/bootstrap/dist/js/bootstrap.min.js') !!}
+{!! Html::script('bower_components/data-tables/media/js/jquery.dataTables.min.js') !!}
+{!! Html::script('bower_components/data-tables/media/js/dataTables.bootstrap.min.js') !!}
 {!! Html::script('bower_components/knockout/dist/knockout.js') !!}
 {!! Html::script('bower_components/bootstrap-combobox/js/bootstrap-combobox.js') !!}
 {!! Html::script('bower_components/datePicker/js/bootstrap-datepicker.js') !!}
 {!! Html::script('bower_components/Chart.js/Chart.js') !!}
+{!! Html::script('js/app.min.js') !!}
 
-	<script type="text/javascript">	    $(document).ready(function(){		    $('[data-toggle="tooltip"]').tooltip();		});		$.ajaxSetup({	        headers: {	            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')	        }	    });    </script>	@if($errors->has())		<script type="text/javascript">	    	$(document).ready(function(){				$("#myModal-error").modal('show');			});       </script>    @endif    @if (Session::has('message'))		<script type="text/javascript">	    	$(document).ready(function(){				$("#myModal-message").modal('show');			});       </script>	@endif
+<script type="text/javascript">    $(document).ready(function(){	    $('[data-toggle="tooltip"]').tooltip();	});	$.ajaxSetup({        headers: {            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')        }    });	@if($errors->has())    	$(document).ready(function(){			$("#myModal-error").modal('show');		});	@endif	@if (Session::has('message'))    	$(document).ready(function(){			$("#myModal-message").modal('show');		});	@endif</script>
