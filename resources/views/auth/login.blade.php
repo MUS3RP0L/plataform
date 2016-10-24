64b8a291c996d2451d8ff0993d27b2a6ededc1c3
aleguis@icloud.com <aleguis@icloud.com>
2016-10-24 11:18:48 -0400

    working on view login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -24,30 +24,34 @@
                         </p>
                     </div>
                     <div class="modal-footer">
-                        <button type="button" class="btn btn-outline" data-dismiss="modal">Cerrar</button>
+                        <div class="row text-center">
+                        	<button type="button" class="btn btn-raised btn-default" data-dismiss="modal">Cerrar</button>
+                        </div>
                     </div>
                 </div>
             </div>
 		</div>
 
-        <div class="login-box-body">
-            {!! Form::open(['url' => 'login', 'role' => 'form']) !!}
-                <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                <div class="form-group has-feedback">
-                    <input type="text" class="form-control" placeholder="Nombre de Usuario" name="username" required/>
-                    <span class="glyphicon glyphicon-user form-control-feedback"></span>
-                </div>
-                <div class="form-group has-feedback">
-                    <input type="password" class="form-control" placeholder="Contraseña" name="password" required/>
-                    <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-                </div>
-                <div class="row">
-                    <div class="col-xs-12">
-                        <button type="submit" class="btn btn-block btn-raised btn-primary">Ingresar</button>
+        <div class="box box-success">
+            <div class="login-box-body">
+                {!! Form::open(['url' => 'login', 'role' => 'form']) !!}
+                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                    <div class="form-group has-feedback">
+                        <input type="text" class="form-control" placeholder="Nombre de Usuario" name="username" required/>
+                        <span class="glyphicon glyphicon-user form-control-feedback"></span>
                     </div>
-                </div>
-            {!! Form::close() !!}
-        </div><!-- /.login-box-body -->
+                    <div class="form-group has-feedback">
+                        <input type="password" class="form-control" placeholder="Contraseña" name="password" required/>
+                        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <button type="submit" class="btn btn-block btn-raised btn-primary">Ingresar</button>
+                        </div>
+                    </div>
+                {!! Form::close() !!}
+            </div>
+        </div>
 
     </div><!-- /.login-box -->
 
